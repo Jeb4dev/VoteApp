d7de7903e65da8332d2c21dc349baa7d2e50ab21
JesperKauppinen <jesperkauppinen@gmail.com>
2021-11-11 15:15:54 +0200

    postresql

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from os import path, environ
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -10,7 +10,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ADD_SECRET_KEY'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', f'sqlite:///{DB_NAME}')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from views import views
